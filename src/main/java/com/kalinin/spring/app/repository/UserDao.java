67d380e309115158086c575d16114f432642fbd7
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-04-13 11:42:08 +0400

    Добавил DAO слой

---
+++ src/main/java/com/kalinin/spring/app/repository/UserDao.java
@@ -0,0 +1,5 @@
+package com.kalinin.spring.app.repository;
+
+public interface UserDao {
+
+}
---
+++ src/main/java/com/kalinin/spring/app/repository/UserDaoImpl.java
@@ -0,0 +1,7 @@
+package com.kalinin.spring.app.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+}
