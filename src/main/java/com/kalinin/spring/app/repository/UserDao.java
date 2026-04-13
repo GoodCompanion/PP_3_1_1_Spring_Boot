b4bd646b1deadaf2673ad181787b197e22f22834
Vladimir Kalinin <mr.vladimar.kalinin2000@mail.ru>
2026-04-13 12:42:32 +0400

    Реализовал сервисный слой

--- src/main/java/com/kalinin/spring/app/repository/UserDao.java
+++ src/main/java/com/kalinin/spring/app/repository/UserDao.java
@@ -1,10 +1,10 @@
 package com.kalinin.spring.app.repository;
 
 import com.kalinin.spring.app.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserDao extends CrudRepository<User, Long> {
-
+public interface UserDao extends JpaRepository<User, Long> {
+    User getUserById(Long id);
 }
--- src/main/java/com/kalinin/spring/app/service/UserService.java
+++ src/main/java/com/kalinin/spring/app/service/UserService.java
@@ -1,4 +1,17 @@
 package com.kalinin.spring.app.service;
 
+import com.kalinin.spring.app.model.User;
+
+import java.util.List;
+
 public interface UserService {
+    void addUser(User user);
+
+    User getUser(Long id);
+
+    List<User> getUsers();
+
+    void updateUser(User user);
+
+    void deleteUser(Long id);
 }
